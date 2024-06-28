6aba709fe09e5c66e54debefdc7a5a52ea632987
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-06-28 10:27:57 +0200

    Fix: changed styles in css files

--- JS/components/pagination.js
+++ JS/components/pagination.js
@@ -1,11 +1,17 @@
 export const pagination = () => {
     const nextButton = document.getElementById('nextPageMain');
     const backButton = document.getElementById('backPage');
-    const mainPage = document.getElementsByClassName('--main');
+    const mainPage = document.getElementById('mainPage')
+    const profilePage = document.getElementById('profilePage')
+    const addressPage = document.getElementById('addressPage')
+    const shippingPage = document.getElementById('shippingPage')
+    const finishPage = document.getElementById('finishPage')
+    const thankPage = document.getElementById('thankPage')
 
     if (nextButton) {
         nextButton.addEventListener('click', () => {
-
+            mainPage.classList.add('hidden')
+            profilePage.classList.remove('hidden')
         });
     }
 
